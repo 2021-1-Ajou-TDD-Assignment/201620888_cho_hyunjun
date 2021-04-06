42b654b1f2507b7387a2fa3be31fee06f5f146bd
hyunjun cho <gusrkfl0609@ajou.ac.kr>
2021-04-06 20:02:11 +0900

    third test

--- test/BowlingGameTest.java
+++ test/BowlingGameTest.java
@@ -13,7 +13,7 @@ public class BowlingGameTest {
 	
 	@Test
 	public void testGutterGame() {
-		
+		setUp();
 		for(int i = 0; i < 20; i++) {
 			g.roll(0);
 		}
@@ -22,11 +22,11 @@ public class BowlingGameTest {
 	
 	@Test
 	public void testAllOnes() {
+		setUp();
 		for(int i = 0; i < 20; i++) {
 			g.roll(1);
 		}
 		assertEquals(20, g.score());
 	}
-
 
 }
