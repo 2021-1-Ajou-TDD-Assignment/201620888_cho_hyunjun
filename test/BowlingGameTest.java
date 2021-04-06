766c6c5db0f78aaa19e260332ed8aa52ae201f47
hyunjun cho <gusrkfl0609@ajou.ac.kr>
2021-04-06 21:18:28 +0900

    add rollSapre function and isSpare function

--- test/BowlingGameTest.java
+++ test/BowlingGameTest.java
@@ -32,10 +32,13 @@ public class BowlingGameTest {
 	@Ignore("until we walk frame by frame")
 	@Test
 	public void testOneSpare() {
-		g.roll(5);
-		g.roll(5);	//spare
+		rollSpare();	//spare
 		g.roll(3);
 		rollMany(17, 0);
 		assertEquals(16, g.score());
+	}
+	private void rollSpare() {
+		g.roll(5);
+		g.roll(5);
 	}
 }
