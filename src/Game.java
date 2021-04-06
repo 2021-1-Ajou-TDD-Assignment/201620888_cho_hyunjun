31b6dc57cbfd00950222243690314e246b835062
hyunjun cho <gusrkfl0609@ajou.ac.kr>
2021-04-06 20:56:22 +0900

    add assertEquals function test

--- src/Game.java
+++ src/Game.java
@@ -1,31 +1,14 @@
 
 public class Game {
 
-	private int rolls[] = new int[21];
-	private int currentRoll = 0;
-	
 	public void roll(int pins) {
-		rolls[currentRoll++] = pins;
+		// TODO Auto-generated method stub
+		
 	}
 
 	public int score() {
-		int score = 0;
-		int frameIndex = 0;
-		for(int frame = 0; frame < rolls.length; frame++) {
-			if(isSpare(frameIndex)) {	//spare
-				score += 10 + rolls[frameIndex+2];
-				frameIndex += 2;
-			} else {
-				score += rolls[frameIndex] + rolls[frameIndex+1];
-				frameIndex += 2;
-			}
-			
-		}
-		return score;
-	}
-
-	private boolean isSpare(int frameIndex) {
-		return rolls[frameIndex] + rolls[frameIndex+1] == 10;
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
 }
--- test/BowlingGameTest.java
+++ test/BowlingGameTest.java
@@ -1,51 +1,16 @@
 import static org.junit.Assert.*;
-import org.junit.Ignore;
 
 import org.junit.Test;
 
-@SuppressWarnings("unused")
 public class BowlingGameTest {
 	
-	private Game g;
-	
-	@Before
-	public void setUp() {
-		g = new Game();
-	}
-	private void rollMany(int n, int pins) {
-		for(int i = 0; i < n; i++) {
-			g.roll(pins);
-		}
-	}
 	@Test
 	public void testGutterGame() {
-		setUp();
-		rollMany(20, 0);
-		assertEquals(0, g.score());
+		Game g = new Game();
+		for(int i = 0; i < 20; i++) {
+			g.roll(0);
+			assertEquals(0, g.score());
+		}
 	}
 	
-	@Test
-	public void testAllOnes() {
-		setUp();
-		rollMany(20, 1);
-		assertEquals(20, g.score());
-	}
-	
-	
-	@Test
-	public void testOneSpare() {
-		setUp();
-		rollSpare();	//spare
-		g.roll(3);
-		rollMany(17, 0);
-		assertEquals(16, g.score());
-	}
-	private void rollSpare() {
-		g.roll(5);
-		g.roll(5);
-	}
-	
-	
-	
-
 }
