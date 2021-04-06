9081a54dad7c76d270398c127c3daed135a33d63
hyunjun cho <gusrkfl0609@ajou.ac.kr>
2021-04-06 21:01:27 +0900

    add setUp function and score local variable

--- src/Game.java
+++ src/Game.java
@@ -1,14 +1,13 @@
 
 public class Game {
-
+	private int score = 0;
 	public void roll(int pins) {
-		// TODO Auto-generated method stub
+		score += pins;
 		
 	}
 
 	public int score() {
-		// TODO Auto-generated method stub
-		return 0;
+		return score;
 	}
 
 }
